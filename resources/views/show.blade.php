b741e1fcdf7210ce3986cead559817b38236f41c
benn <benncadproost@gmail.com>
2021-01-18 02:00:34 +0100

    front end date on details page

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -9,8 +9,8 @@
                     <img src="{{$picture->url}}" class="mt-3 mb-4" style="max-width: 400px;">
                     @else
                     <iframe width="800" height="600" src="{{$picture->url}}" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen class="mt-3 mb-4"></iframe>
-                    <h5>{{$picture->date}}</h5>
                     @endif
+                    <h5>{{$picture->date}}</h5>
                     <h1 class="jumbotron-heading">{{$picture->title}}</h1>
                     @if (!empty($picture->copyright))
                     <p class="lead"> 
